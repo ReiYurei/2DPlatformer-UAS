720d6ab5b8991903698f7f926645711f4e9a8262
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2022-07-25 18:11:50 +0700

    Camera follow tweaked
    -Camera follow tweaked so it wont track
    horizontal movement
    -Playtest placeholder extended

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -10,7 +10,7 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 6f; 
     void FixedUpdate()
     {
-        Vector3 desiredPos = target.position + offset;
+        Vector3 desiredPos = new Vector3(0, target.position.y, offset.z);
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPos;
     }
