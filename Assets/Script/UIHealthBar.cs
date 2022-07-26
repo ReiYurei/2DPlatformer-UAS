4ee2bf54ae4da79e4b29d82c560f98816e4599b4
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2022-07-27 03:17:38 +0700

    Rewrite some vanishing code due to merge
    -Rewrite Jump Bar due to merge

--- Assets/Script/UIHealthBar.cs
+++ Assets/Script/UIHealthBar.cs
@@ -5,18 +5,34 @@ using UnityEngine.UI;
 
 public class UIHealthBar : MonoBehaviour
 {
-   
+
+    public PlayerMoveController player;
     public UnitStatus status;
     public Slider slider;
+    public Slider sliderHP;
+    public Slider sliderJump;
+
+    public Gradient gradient;
+    public Image fill;
 
     void Update()
     {
-        SetHealth();     
-    }
-    public void SetHealth()
-    {
-      //  slider.highValue = status.maxHP;
-      //  slider.value = status.HealthPoint;
+        SetHealth();
+        SetJump();
     }
 
+    public void SetJump()
+    {
+        slider.maxValue = status.maxHP;
+        slider.value = status.HealthPoint;
+        sliderJump.maxValue = 2;
+        sliderJump.value = player.CurrentAttackCount;
+        fill.color = gradient.Evaluate(sliderJump.normalizedValue);
+    }
+
+    public void SetHealth()
+    {
+        sliderHP.maxValue = status.maxHP;
+        sliderHP.value = status.HealthPoint;
+    }
 }
